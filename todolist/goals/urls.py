d5ea3e580e7e342baea96a317829c7b2774ef908
DanilMirosh <miroshnichenko.da.vi@mail.ru>
2023-01-27 15:53:03 +0500

    fix goal url

--- todolist/goals/urls.py
+++ todolist/goals/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('goal_category/create', views.GoalCategoryCreateView.as_view(), name='create-category'),
-    path('goal_category/list', views.GoalCategoryListView.as_view(), name='list-categories'),
-    path('goal_category/<pk>', views.GoalCategoryView.as_view(), name='retrieve-update-destroy-category'),
+    path('goal_category/create', views.GoalCategoryCreateView.as_view(), name='category-create'),
+    path('goal_category/list', views.GoalCategoryListView.as_view(), name='category-list'),
+    path('goal_category/<pk>', views.GoalCategoryView.as_view(), name='category-detail'),
 
     path('goal/create', views.GoalCreateView.as_view(), name='create-goal'),
     path('goal/list', views.GoalListView.as_view(), name='goal-list'),
