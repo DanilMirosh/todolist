9ad79d75906088261404c129a0f426b5ac41c2b9
DanilMirosh <miroshnichenko.da.vi@mail.ru>
2023-01-30 21:57:34 +0500

    fix bot model

--- todolist/bot/models.py
+++ todolist/bot/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
+from todolist.core.models import User
+
 
 class TgUser(models.Model):
-    tg_id = models.BigIntegerField(verbose_name='Chat ID')
-    username = models.CharField()
+    chat_id = models.BigIntegerField(verbose_name='Chat ID', unique=True)
+    username = models.CharField(verbose_name='Username', max_length=255, null=True, blank=True, default=None)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, max_length=32, null=True, default=None)
+    verification_code = models.CharField(max_length=32, null=True, blank=True, default=None)
