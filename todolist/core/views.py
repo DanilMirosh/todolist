90a518b5cef0a0b8965a5261a6393d3a9ac2243d
DanilMirosh <miroshnichenko.da.vi@mail.ru>
2023-02-06 22:50:27 +0500

    add docstring views core

--- todolist/core/views.py
+++ todolist/core/views.py
@@ -6,13 +6,16 @@ from rest_framework.response import Response
 
 
 class SignupView(generics.CreateAPIView):
+    """Ручка для регистрации нового пользователя"""
     serializer_class = CreateUserSerializer
 
 
 class LoginView(generics.CreateAPIView):
+    """Ручка для входа пользователя"""
     serializer_class = LoginSerializer
 
     def create(self, request, *args, **kwargs):
+        """Метод производит вход(login) пользователя"""
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         login(request=self.request, user=serializer.save())
@@ -20,20 +23,25 @@ class LoginView(generics.CreateAPIView):
 
 
 class ProfileView(generics.RetrieveUpdateDestroyAPIView):
+    """Ручка для отображения, редактирования и выхода пользователя"""
     queryset = User.objects.all()
     serializer_class = ProfileSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_object(self):
+        """Метод возвращает объект пользователя из БД"""
         return self.request.user
 
     def perform_destroy(self, instance):
+        """Метод производит выход(logout) пользователя"""
         logout(self.request)
 
 
 class UpdatePasswordView(generics.UpdateAPIView):
+    """Ручка для смены пароля пользователя"""
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = UpdatePasswordSerializer
 
     def get_object(self):
+        """Метод возвращает объект пользователя из БД"""
         return self.request.user
