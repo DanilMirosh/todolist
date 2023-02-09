556fbbbc4e68039fafb7ac9f818f911270908963
DanilMirosh <miroshnichenko.da.vi@mail.ru>
2023-02-09 14:56:30 +0500

    add fixtur

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -23,3 +23,63 @@ def create_login_user(client):
     )
 
     return create_user_response, login_user_response
+
+
+@pytest.fixture
+def create_another_user(client):
+    """Фикстура создания и логин второго пользователя"""
+    user_data = {
+        'username': 'test2',
+        'first_name': 'Test',
+        'last_name': 'test',
+        'email': 'test@test.ru',
+        'password': 'test12234567',
+        'password_repeat': 'test12234567'
+    }
+
+    create_user_response = client.post(
+        '/core/signup',
+        data=user_data,
+        content_type='application/json')
+
+    return create_user_response
+
+
+@pytest.fixture
+def create_board(client, create_login_user):
+    """Фикстура создания доски"""
+    create_board_response = client.post(
+        '/goals/board/create',
+        data={'title': 'test_board'},
+        content_type='application/json'
+    )
+    return create_board_response
+
+
+@pytest.fixture
+def create_category(client, create_board):
+    """Фикстура создания категории"""
+    create_category = client.post(
+        '/goals/goals_category/create',
+        {
+            'title': 'test_category',
+            'board': create_board.data['id']
+        },
+        format='json'
+    )
+    return create_category
+
+
+@pytest.fixture
+def create_goal(client, create_category):
+    """Фикстура создания цели"""
+    create_goal = client.post(
+        '/goals/goal/create',
+        {
+            'title': 'test_goal',
+            'category': create_category.data['id']
+        },
+        content_type='application/json'
+    )
+
+    return create_goal
